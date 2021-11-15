09b6b25dfc8422c98ca8a3f19759a1e7efad36bf
Albert Andersson <andersson.albert@gmail.com>
2021-11-15 14:56:40 +0100

    POST, GET, GET(ALL) and Delete implemented

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,7 +1,70 @@
 package se.iths.rest;
 
+import se.iths.entity.Student;
+import se.iths.exception.StudentNotFoundException;
+import se.iths.service.StudentService;
 
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+@Path("students")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class StudentRest {
 
+    StudentService studentService;
 
+    @Inject
+    public StudentRest(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @Path("")
+    @POST
+    public Response createStudent(Student student, @Context UriInfo uriInfo) {
+
+        studentService.createStudent(student);
+
+        var createdUri =
+                uriInfo.getBaseUriBuilder().path(student.getId().toString()).build();
+
+        return Response.created(createdUri).build();
+    }
+
+//    @Path("{id}")
+//    public Response updateStudent(Student student) {
+//        // Todo Implement UPDATE
+//        return null;
+//    }
+//
+    @Path("{id}")
+    @GET
+    public Response getStudent(@PathParam("id") Long id) {
+        var foundStudent = studentService.findById(id);
+        if(foundStudent != null)
+            return Response.ok(foundStudent).build();
+
+        throw new StudentNotFoundException(id);
+    }
+
+    @Path("")
+    @GET
+    public Response getAllStudents() {
+        var allStudents = studentService.getAll();
+
+        return Response.ok(allStudents).build();
+    }
+
+    @Path("{id}")
+    @DELETE
+    public Response deleteStudent(@PathParam("id") Long id) {
+        if (studentService.delete(id)) {
+            return Response.ok().build();
+        }
+        throw new StudentNotFoundException(id);
+    }
 }
--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -1,7 +1,40 @@
 package se.iths.service;
 
 
+import se.iths.entity.Student;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
 public class StudentService {
 
+    @PersistenceContext
+    EntityManager em;
 
+
+    public void createStudent(Student student) {
+        em.persist(student);
+    }
+
+    public Student findById(Long id) {
+        return em.find(Student.class, id);
+    }
+
+    public List<Student> getAll() {
+        TypedQuery<Student> getAllStudents = em.createQuery("SELECT s from Student s", Student.class);
+        return getAllStudents.getResultList();
+    }
+
+    public boolean delete(Long id) {
+        var studentToDelete = findById(id);
+        if(studentToDelete != null) {
+            em.remove(studentToDelete);
+            return true;
+        }
+        else return false;
+    }
 }
