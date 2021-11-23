3ac5cf8d4cd6e660895eb4e81483da188ae03bf7
Albert Andersson <andersson.albert@gmail.com>
2021-11-23 15:47:09 +0100

    Clarified error message

--- src/main/java/se/boalbert/exception/SubjectNotFoundException.java
+++ src/main/java/se/boalbert/exception/SubjectNotFoundException.java
@@ -7,7 +7,7 @@ import javax.ws.rs.core.Response;
 public class SubjectNotFoundException extends WebApplicationException {
     public SubjectNotFoundException(String message) {
         super(Response.status(Response.Status.NOT_FOUND)
-                .entity(new SubjectNotFoundMessage(message + " was not found."))
+                .entity(new SubjectNotFoundMessage(message + " subject was not found."))
                 .type(MediaType.APPLICATION_JSON)
                 .build());
     }
